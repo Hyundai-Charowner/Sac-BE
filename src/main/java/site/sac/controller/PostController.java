1eec876009349483aaa564e28829cf753e41e07e
Eunku Kang <kegoo1997@gmail.com>
2023-12-11 11:34:49 +0900

    feat:post api(게시글 조회, 등록)

--- src/main/java/site/sac/controller/PostController.java
+++ src/main/java/site/sac/controller/PostController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.sac.dto.PostDTO;
 import site.sac.mapper.PostMapper;
 import site.sac.service.PostService;
@@ -21,6 +18,17 @@ import java.util.Map;
 public class PostController {
     @Autowired
     private PostService postService;
+
+    @PostMapping("/posts")
+    public String postInsert(PostDTO postDTO){
+        try {
+            postService.register(postDTO);
+            return "sucess";
+        }catch (Exception e){
+            return "fail";
+        }
+    }
+
     @GetMapping("/posts")
     public ResponseEntity<Map<String,Object>> getAllPost(){
         List<PostDTO> posts = postService.getAllPost();
