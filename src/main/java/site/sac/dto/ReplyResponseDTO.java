2df57cd6911ef84ddfaad90bd511e1677c3d9dae
EunKu <kegoo1997@gmail.com>
2023-12-14 15:43:44 +0900

    fix : reply api 처리

--- src/main/java/site/sac/dto/ReplyResponseDTO.java
+++ src/main/java/site/sac/dto/ReplyResponseDTO.java
@@ -4,11 +4,7 @@ import lombok.Data;
 
 @Data
 public class ReplyResponseDTO {
-    private long reply_id;
-    private long post_id;
-    private long user_id;
     private String reply_content;
-
     private String user_name;
     private String user_image;
 }
