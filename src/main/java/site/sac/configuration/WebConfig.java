ae7b01b920b7e6028f9abae50ae39486c719dd5a
= <ptibabmgo@gmail.com>
2023-12-14 17:04:57 +0900

    fix: cors 수정

--- src/main/java/site/sac/configuration/WebConfig.java
+++ src/main/java/site/sac/configuration/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://sac.monster")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
