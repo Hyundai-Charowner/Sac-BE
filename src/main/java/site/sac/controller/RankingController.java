5cff8e981dbccfea930e007d8c8b408d398eba6c
EunKu <kegoo1997@gmail.com>
2023-12-14 16:47:50 +0900

    feature-ranking기능 추가

--- src/main/java/site/sac/controller/RankingController.java
+++ src/main/java/site/sac/controller/RankingController.java
@@ -2,6 +2,7 @@ package site.sac.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,12 @@ public class RankingController {
     @GetMapping("/topic")
     public ResponseEntity<List<String>> getTopicRanking() throws DataAccessException {
         List<String> result = rankingService.getTopicRanking();
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     @GetMapping("/passion")
     public ResponseEntity<List<String>> getPassionRankingt() throws DataAccessException, NullPointerException {
         List<String> result = rankingService.getPassionRanking();
-        return ResponseEntity.status(200).build();
+        return ResponseEntity.status(200).body(result);
     }
 }
